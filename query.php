a8b44d4e1e448901b07c0b770352878dba93e431
Natalie Wowk <natalie.wowk@gmail.com>
2012-03-08 20:34:11 -0500

    queries are fun

--- query.php
+++ query.php
@@ -7,11 +7,39 @@ session_start();
 <head>
 <?php
 require 'includes/header.ssi';
+
+if ( isset($_POST['district'])) {
+	$district = mysql_real_escape_string($_POST['district']);
+	$query = "SELECT * FROM households WHERE district=$district";
+	$result = mysql_query($query);
+	if ( $result == FALSE ) {
+        	$_SESSION['error'] = 'No data for the district you entered.';
+        	return;
+		}
+//This part is where we calculate all the general stats for the district.
+	$hhlds = mysql_num_rows($result);
+
+//display profile information
+	echo ('<h2>Profile for District '.$district.':</h2>');
+	echo ('<p>There are '.$hhlds.' household entries in this district.</p>');
+
+	$query = 'SELECT district, SUM(stories) FROM households GROUP BY district';
+	$result = mysql_query($query);
+while($row = mysql_fetch_array($result)){
+	echo "District ". $row['district']. " has ". $row['SUM(stories)']." stories.";
+	echo "<br />";
+}
+}
 ?>
 </head>
 <body id="home">
 <h1>Search</h1>
 <p>This page will have functionalities to query the database, but is not complete at this time.
     We hope to allow a variety of queries that allow multiple options for display of data.</p>
+<form method="post">
+<p>For which district would you like a profile?
+<input type="text" name="district">
+<input type="submit" value="Go"/>
+</form>
 </body>
 </html>
