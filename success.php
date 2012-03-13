46c73384925f09187bf6b1fed397034eae212cc6
Olivia Lau <olau@umich.edu>
2012-03-13 17:35:51 -0400

    modified session start

--- success.php
+++ success.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require_once "db.php";
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -10,8 +14,6 @@
 </head>
 <body id="home">
 <?php
-session_start();
-require_once "db.php";
 
 if ( isset($_SESSION ['name']) ) 
 {
