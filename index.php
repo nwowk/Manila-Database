6c694c5d9167c30e0461d917f263a8347adf63f4
Olivia Lau <olau@umich.edu>
2012-03-13 13:29:01 -0400

    working 4 me!

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php
+session_start();
+require_once "db.php";
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <link href="main.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
 </style>
-<?php
-require_once "db.php";
 
-?>
 <head>
 <title>Manila Database</title>
 <?php
@@ -16,8 +17,6 @@ require 'includes/header.ssi';
 </head>
 <body  id="home">
 <?php
-session_start();
-require_once "db.php";
 
 if ( isset($_SESSION ['name']) ) 
 {
