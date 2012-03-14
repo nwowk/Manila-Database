8937f3e69ece1d0964585f22f085fc395f4eea9d
Olivia Lau <olau@umich.edu>
2012-03-14 18:29:09 -0400

    ydtdxy

---
+++ guard.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once "db.php";
+//check for correct login, otherwise redirects
+//if (isset($_SESSION['name']) && $_SESSION['name']) {
+//    header('Location: login.php' . $_SESSION['name']); 
+//    exit; //header redirect
+//}
+
+//if (!isset($_SESSION['name'])) || $_SESSION['name'] != "granted") {
+//  header('Location: index.php');
+//  exit();
+//}
+//if loginin
+//redirect
+//return
+if(!$session->'name'()){
+    redirect_to("index.php"); }
+
+?>
