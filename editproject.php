5f810061778fd2ef7ee7c7f9d2417f09f7e13bbf
Olivia Lau <olau@umich.edu>
2012-03-14 18:52:36 -0400

    fixing session start

--- editproject.php
+++ editproject.php
@@ -1,9 +1,9 @@
+<?php
+session_start();
+require_once "db.php";
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<?php
-require_once "db.php";
-session_start();
-?>
 <head>
 <?php
 require 'includes/header.ssi';
