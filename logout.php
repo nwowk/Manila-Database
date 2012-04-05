3a88aae8ac33a9957d45afa6e358742d3e038485
Olivia Lau <olau@umich.edu>
2012-04-04 22:18:58 -0400

    cleared session in verify
    in the verify page, it no longer shows the last record if we try to
    access an entry. Issue was fixed in the logout, which clears the
    session after a user logs-out.

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-unset ($_SESSION['name']);
+//unset ($_SESSION['name']);
+
+session_regenerate_id();
+session_destroy();
+unset($_SESSION);
 
 header ( 'Location: success.php'); 
 
